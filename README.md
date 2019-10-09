3d163fbcef9c0b60b24ae5572f17b25076e79e67
Oded Sharon <odedshr@gmail.com>
2019-10-09 22:16:23 +0100

    Added files

---
+++ README.md
@@ -0,0 +1,33 @@
+# assetify
+Use Chokidar to watch multiple sources and copy to multiple targets
+
+## Installing
+```
+npm i -D assetify
+```
+
+## Setting up
+Add an `assets` section to your `package.json` with an array of element; Each element should have `source` and `target - that can be either single item or an array of items
+```
+  "assets": [
+    {
+      "source": [
+        "inputs/dir1",
+        "inputs/dir2"
+      ],
+      "target": "outputs/out1"
+    },
+    {
+      "source": "inputs/dir3",
+      "target": [
+        "outputs/out1",
+        "outputs/out2"
+      ]
+    }
+  ]
+  ```
+  
+  ## Running
+  ```
+  node ./node_modules/assetify/index.js
+  ```
---
+++ index.js
@@ -0,0 +1,86 @@
+const { watch } = require('chokidar');
+const { assets } = require('./package.json');
+
+const fs = require('fs');
+const path = require('path');
+
+assets.map(entry => {
+	const targets = toArray(entry.target);
+
+	return toArray(entry.source).map(source => {
+		return watch(source)
+			.on('add', path => copy(source, path, targets))
+			.on('change', path => copy(source, path, targets))
+			.on('unlink', path => remove(source, path, targets))
+			.on('addDir', path => source !== path && copy(source, path, targets))
+			.on('unlinkDir', path => remove(source, path, targets));
+	});
+});
+
+function toArray(element) {
+	return Array.isArray(element) ? element : [element];
+}
+
+function getTargetPath(source, file, target) {
+	return file.replace(source, target);
+}
+
+function remove(source, file, targets) {
+	targets.forEach(target => console.log(removeFileSync(getTargetPath(source, file, target))));
+}
+
+function copy(source, file, targets) {
+	targets.forEach(target => console.log(copyFolderRecursiveSync(file, getTargetPath(source, file, target))));
+}
+
+function copyFileSync(source, target) {
+	let targetFile = target;
+
+	//if target is a directory a new file with the same name will be created
+	if (fs.existsSync(target)) {
+		if (fs.lstatSync(target).isDirectory()) {
+			targetFile = path.join(target, path.basename(source));
+		}
+	}
+
+	fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function verifyFolderExists(folder) {
+	if (!fs.existsSync(folder)) {
+		fs.mkdirSync(folder);
+	}
+}
+
+function copyFolderRecursiveSync(source, target) {
+	if (fs.lstatSync(source).isDirectory()) {
+		verifyFolderExists(target);
+
+		fs.readdirSync(source).forEach(file => {
+			const curSource = path.join(source, file);
+
+			if (fs.lstatSync(curSource).isDirectory()) {
+				copyFolderRecursiveSync(curSource, target);
+			} else {
+				copyFileSync(curSource, target);
+			}
+		});
+	} else {
+		copyFileSync(source, target);
+	}
+
+	return `copying ${source} => ${target}`;
+}
+
+function removeFileSync(path) {
+	if (fs.existsSync(path)) {
+		if (fs.lstatSync(path).isDirectory()) {
+			fs.readdirSync(path).forEach(file => removeFileSync(`${path}/${file}`));
+			fs.rmdirSync(path);
+		} else {
+			fs.unlinkSync(path); // delete file
+		}
+	}
+
+	return `removing ${path}`;
+}
